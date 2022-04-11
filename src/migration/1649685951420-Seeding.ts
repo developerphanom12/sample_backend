0bd49adc5b75b42aa6245872aea9d8e062567149
Nikita Korotun <nkorotun@codempire.team>
2022-04-11 17:17:11 +0300

    add-initial-currency

--- src/migration/1649685951420-Seeding.ts
+++ src/migration/1649685951420-Seeding.ts
@@ -1,12 +1,10 @@
 import { CurrencySeed } from 'src/constants/seed';
 import { getRepository, MigrationInterface, QueryRunner } from 'typeorm';
 
-export class Seeding1649675153104 implements MigrationInterface {
+export class Seeding1649685951420 implements MigrationInterface {
   public async up(_: QueryRunner): Promise<void> {
     await getRepository('currency').save(CurrencySeed);
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {
-    //
-  }
+  public async down(_: QueryRunner): Promise<void> {}
 }
