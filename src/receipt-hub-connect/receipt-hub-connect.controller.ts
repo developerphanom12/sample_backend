2ca6f65571471f5be1391714e96dbd0e25a6b735
Vadim <vadim.dyachenko.2018@ukr.net>
2022-11-09 20:19:45 +0200

    feature/capium-parse-receipt

--- src/receipt-hub-connect/receipt-hub-connect.controller.ts
+++ src/receipt-hub-connect/receipt-hub-connect.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Headers,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FilesInterceptor } from '@nestjs/platform-express';
 import {
   ApiBearerAuth,
   ApiOperation,
@@ -7,6 +17,10 @@ import {
 } from '@nestjs/swagger';
 
 import { LoginDTO } from '../auth/dto/login.dto';
+import {
+  RECEIPT_PHOTOS_LIMIT,
+  RECEIPT_SWAGGER,
+} from '../receipt/receipt.constants';
 import {
   RH_CONNECT_ROUTES,
   RH_CONNECT_SWAGGER,
@@ -19,6 +33,18 @@ import { IRHconnect } from './receipt-hub-connect.types';
 @Controller(RH_CONNECT_ROUTES.main)
 export class ReceiptHubConnectController {
   constructor(private readonly rhConnectService: ReceiptHubConnectService) {}
+
+  @Post(RH_CONNECT_ROUTES.parse)
+  @UseInterceptors(FilesInterceptor('receipt_photos', RECEIPT_PHOTOS_LIMIT))
+  @ApiOperation({ summary: RH_CONNECT_ROUTES.parse })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: RECEIPT_SWAGGER.success,
+  })
+  @HttpCode(HttpStatus.OK)
+  public async creteReceipt(@UploadedFiles() files) {
+    return await this.rhConnectService.parseReceipt(files);
+  }
 
   @Post(RH_CONNECT_ROUTES.rh_connect)
   @ApiOperation({ summary: RH_CONNECT_SWAGGER.rh_connect })
--- src/receipt-hub-connect/receipt-hub-connect.service.ts
+++ src/receipt-hub-connect/receipt-hub-connect.service.ts
@@ -9,6 +9,17 @@ import { LoginDTO } from '../auth/dto/login.dto';
 import { AuthEntity } from '../auth/entities/auth.entity';
 import { EXPIRE_CAPIUM_JWT_TIME } from '../constants/jwt';
 import { IRHconnect } from './receipt-hub-connect.types';
+import { S3Service } from '../s3/s3.service';
+import { DownloadService } from '../download/download.service';
+import {
+  extractCurrency,
+  extractDate,
+  extractNet,
+  extractSupplier,
+  extractTotal,
+  extractVat,
+} from '../heplers/receipt.helper';
+import { EReceiptStatus } from '../receipt/receipt.constants';
 
 @Injectable()
 export class ReceiptHubConnectService {
@@ -17,7 +28,32 @@ export class ReceiptHubConnectService {
     private authRepository: Repository<AuthEntity>,
     private configService: ConfigService,
     private jwtService: JwtService,
+    private s3Service: S3Service, // private downloadService: DownloadService,
   ) {}
+
+  async getImageDataForCapium(photo: string) {
+    const photoPath = await this.uploadPhotoToBucket(photo);
+    try {
+      const { imageName, textractImage } = await this.textractImage(photoPath);
+      const data = await this.extractData(textractImage);
+      await this.deleteImage(imageName);
+
+      return data;
+    } catch (err) {
+      console.log('Error', err);
+      return {
+        status: EReceiptStatus.rejected,
+      };
+    }
+  }
+
+  async parseReceipt(photos) {
+    const promises = photos.map((photo) => {
+      return this.getImageDataForCapium(photo);
+    });
+    const textractData = await Promise.all(promises);
+    return textractData;
+  }
 
   async receiptHubConnect(data: LoginDTO): Promise<IRHconnect> {
     const { email, password } = data;
@@ -41,6 +77,78 @@ export class ReceiptHubConnectService {
 
     return {
       access_token,
+    };
+  }
+
+  async extractData(textData: string[]) {
+    const text = textData.join(' ').toLocaleLowerCase();
+    const receiptData = {
+      supplier: extractSupplier(textData[0]),
+      receipt_date: extractDate(text),
+      tax: extractVat(text),
+      total: extractTotal(text),
+      net: extractNet(text),
+      vat_code: null,
+      currency: extractCurrency(text),
+    };
+
+    if (
+      receiptData.total &&
+      receiptData.tax &&
+      (!receiptData.net || receiptData.net === receiptData.total)
+    ) {
+      const totalNet = Math.abs(receiptData.total - receiptData.tax);
+      totalNet > receiptData.total
+        ? (receiptData.net = null)
+        : (receiptData.net = totalNet);
+    }
+
+    if (
+      !(
+        receiptData.receipt_date ||
+        receiptData.total ||
+        receiptData.tax ||
+        receiptData.net
+      )
+    ) {
+      return {
+        ...receiptData,
+        status: EReceiptStatus.rejected,
+      };
+    }
+
+    return {
+      ...receiptData,
+      status: EReceiptStatus.review,
+    };
+  }
+
+  async textractImage(photoPath) {
+    const textractImage = await this.s3Service.textractFile(photoPath);
+    const imageName = photoPath.key.split('/')[2];
+    return { textractImage, imageName };
+  }
+
+  async deleteImage(image_name: string) {
+    try {
+      await this.s3Service.deleteFile(`capium/receipts/${image_name}`);
+      return 'Image Delete Success';
+    } catch (e) {
+      console.log(e);
+      throw new HttpException('IMAGE NOT FOUND', HttpStatus.NOT_FOUND);
+    }
+  }
+
+  async uploadPhotoToBucket(file) {
+    if (!file) {
+      throw new HttpException('NO RECEIPT PHOTO', HttpStatus.BAD_REQUEST);
+    }
+    const folderName = 'capium/receipts';
+    const response = await this.s3Service.loadFile(file, folderName);
+
+    return {
+      link: response.location,
+      key: response.key,
     };
   }
 }
